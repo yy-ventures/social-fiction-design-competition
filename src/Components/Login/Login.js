689917b6979373bce516ed13968da3e82d854f82
Raihan <rishaon0@gmail.com>
2022-08-14 18:10:22 +0600

    login button text change

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,85 +1,84 @@
-import React, { useState } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-import './Login.scss';
+import React, { useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import "./Login.scss";
 
 const Login = () => {
+  const [authUserName, setAuthUserName] = useState("");
+  const [authUserPass, setAuthUserPass] = useState("");
 
-    const [authUserName, setAuthUserName] = useState('')
-    const [authUserPass, setAuthUserPass] = useState('')
-    
-    // Base Url
-    const baseUrl = process.env.REACT_APP_BASE_URL;
+  // Base Url
+  const baseUrl = process.env.REACT_APP_BASE_URL;
 
-    // authenticate && redirect
-    const history = useHistory()
-    const location = useLocation()
-    const { from } = location.state || { from: { pathname: "/sfdc-registration" } }
+  // authenticate && redirect
+  const history = useHistory();
+  const location = useLocation();
+  const { from } = location.state || { from: { pathname: "/sfdc-registration" } };
 
-    const HandleUserName = e => {
-        setAuthUserName(e.target.value)
-    }
-    const HandleUserPass = e => {
-        setAuthUserPass(e.target.value)
-    }
+  const HandleUserName = (e) => {
+    setAuthUserName(e.target.value);
+  };
+  const HandleUserPass = (e) => {
+    setAuthUserPass(e.target.value);
+  };
 
-    const HandleSubmit = e => {
-        e.preventDefault()
+  const HandleSubmit = (e) => {
+    e.preventDefault();
 
-        let headers = new Headers()
-        let formData = new FormData()
+    let headers = new Headers();
+    let formData = new FormData();
 
-        formData.append("username", authUserName)
-        formData.append("password", authUserPass)
+    formData.append("username", authUserName);
+    formData.append("password", authUserPass);
 
-        let requestOptions = {
-            method: "POST",
-            body: formData,
-            redirect: "follow",
-            headers: headers,
-        };
+    let requestOptions = {
+      method: "POST",
+      body: formData,
+      redirect: "follow",
+      headers: headers,
+    };
 
-        fetch(`${baseUrl}/login`, requestOptions)
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.success) {
-                    localStorage.setItem('app_id', authUserName)
-                    localStorage.setItem('name', JSON.stringify(data.data.name))
-                    localStorage.setItem('token', JSON.stringify(data.data.token))
-                    setAuthUserName('')
-                    setAuthUserPass('')
-                    history.replace(from)
-                }
-                if (data.success === false) {
-                    alert('Something went wrong')
-                    setAuthUserName('')
-                    setAuthUserPass('')
-                }
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
+    fetch(`${baseUrl}/login`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          localStorage.setItem("app_id", authUserName);
+          localStorage.setItem("name", JSON.stringify(data.data.name));
+          localStorage.setItem("token", JSON.stringify(data.data.token));
+          setAuthUserName("");
+          setAuthUserPass("");
+          history.replace(from);
+        }
+        if (data.success === false) {
+          alert("Something went wrong");
+          setAuthUserName("");
+          setAuthUserPass("");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-    return (
-        <section className='login-section'>
-            <form onSubmit={HandleSubmit}>
-                <div>
-                    <div className='text-center'>
-                        <h2>SFDC Login</h2>
-                    </div>
-                    <div className='mt-4'>
-                        <input type='text' placeholder='username..' onChange={HandleUserName} required/>
-                    </div>
-                    <div className='mt-4'>
-                        <input type='password' placeholder='password..' onChange={HandleUserPass} required/>
-                    </div>
-                    <div className='mt-4 text-center'>
-                        <button type='submit'>submit</button>
-                    </div>
-                </div>
-            </form>
-        </section>
-    );
+  return (
+    <section className="login-section">
+      <form onSubmit={HandleSubmit}>
+        <div>
+          <div className="text-center">
+            <h2>SFDC Login</h2>
+          </div>
+          <div className="mt-4">
+            <input type="text" placeholder="username.." onChange={HandleUserName} required />
+          </div>
+          <div className="mt-4">
+            <input type="password" placeholder="password.." onChange={HandleUserPass} required />
+          </div>
+          <div className="mt-4 text-center">
+            <button type="submit">Login</button>
+          </div>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
